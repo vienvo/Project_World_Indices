c08d1870c7cf8d5464b423d1486cfb602a476147
Omar-Castillo <omar.castillo09@gmail.com>
2019-06-16 22:32:20 -0500

    heroku postgres db working properly now

--- app.py
+++ app.py
@@ -19,23 +19,17 @@ app = Flask(__name__)
 #################################################
 url = config.url
 #will need to create a config file for heroku database url
-# app.config["SQLALCHEMY_DATABASE_URI"] = url
+app.config["SQLALCHEMY_DATABASE_URI"] = url
 
 #create engine
 engine = create_engine(url)
 
 #helps with caching
-# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = "0"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = "0"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-df = pd.read_csv("Data/Combined_Data.csv")
-
-df.to_sql(name="world_indices",con=engine, if_exists = 'replace', index_label='id')
-
-with engine.connect() as con:
- con.execute('ALTER TABLE world_indices ADD PRIMARY KEY (id);')
-# db = SQLAlchemy(app)
-# db.init_app(app)
+db = SQLAlchemy(app)
+db.init_app(app)
 
 # df = pd.read_sql("SELECT * FROM world_indices", engine)
 
@@ -61,11 +55,11 @@ def names():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    # stmt = db.session.query(world_indices).statement
-    # df = pd.read_sql_query(stmt, db.session.bind)
+    stmt = db.session.query(World_Data).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
 
     # Return a list of the column names (Years Data)
-    return jsonify(list(df.columns)[3:])
+    return jsonify(list(df.columns)[4:])
 
 
 
